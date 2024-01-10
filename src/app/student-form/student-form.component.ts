8afe20702e8b9e2f4dd118d90f6712afa0b64506
nangkyawt <nangkyawthsuhlaing@gmail.com>
2024-01-10 17:26:56 +0630

    validation commit

--- src/app/student-form/student-form.component.ts
+++ src/app/student-form/student-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ApiService } from '../service/api.service';
 import { HttpClient } from '@angular/common/http';
 import { throttle } from 'rxjs';
@@ -10,18 +10,23 @@ import * as XLSX from 'xlsx';
   styleUrls: ['./student-form.component.css'],
 })
 export class StudentFormComponent implements OnInit {
-  data: any;
+  @ViewChild('fileInput') fileInput!: ElementRef;
 
   constructor(private http: HttpClient, private service: ApiService) {}
-  pageSize: any = 5;
+  pageSize: any = 10;
   currentPage = 1;
   searchText: any;
-
+  data: any;
   results: any = [];
   array: any = [];
   excel_data: any = [];
-  // import_data: any = [];
+
   showSuccessMessage = false;
+  showErrorMessage = false;
+  showGenderErrorMessage = false;
+  showNRCExistsErrorMessage = false;
+  showIdErrorMessage = false;
+  showNameErrorMessage = false;
   students = {
     id: 0,
     name: '',
@@ -42,9 +47,169 @@ export class StudentFormComponent implements OnInit {
   genderfemale() {
     this.students.gender = true;
   }
+  // Export to Excel
+  exportToExcel(data: any[], fileName: string): void {
+    // Exclude unwanted columns (e.g., CreatedAt and UpdatedAt)
+    const excludedColumns = ['createdAt', 'updatedAt'];
+    const modifiedData = data.map((item) => {
+      const newItem = { ...item };
+      if (newItem.gender !== undefined) {
+        newItem.gender == newItem.gender ? 'Male' : 'Female';
+      }
+      excludedColumns.forEach((column) => delete newItem[column]);
+      return newItem;
+    });
+    // Convert modified data to Excel worksheet
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(modifiedData);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    // Save the workbook to an Excel file
+    XLSX.writeFile(wb, `${fileName}.xlsx`);
+  }
 
+  // Import from Excel
+  importFromExcel(event: any): void {
+    const file: File = event.target.files[0];
+    const reader: FileReader = new FileReader();
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const data = e.target.result;
+        const binaryString: string = e.target.result;
+        const workbook: XLSX.WorkBook = XLSX.read(binaryString, {
+          type: 'binary',
+        });
+        const sheetName: string = workbook.SheetNames[0];
+        const worksheet: XLSX.WorkSheet = workbook.Sheets[sheetName];
+        const importedData: any[] = XLSX.utils.sheet_to_json(worksheet, {
+          raw: true,
+        });
+
+        for (const row of importedData) {
+          const idSet = new Set();
+
+          // Check if gender property exists and is not null
+          // if (isNaN(row.id) || idSet.has(row.id)) {
+          //         return false;
+          //       } else {
+          //         console.error(
+          //           'Invalid or duplicate id . Must be a unique number or order ID cannot be null'
+          //         );
+          //         this.showIdErrorMessage = true;
+          //         setTimeout(() => {
+          //           this.showIdErrorMessage = false;
+          //         }, 3000);
+          //       }idSet.add(row.id);
+
+          if (typeof row.gender !== 'boolean' || row.gender == null) {
+            // Convert the gender value to a string (if it's not already)
+            // const genderString = String(row.gender).trim().toLowerCase();
+            // console.log(genderString);
+            // if (genderString === 'male' || genderString === 'female') {
+            //   row.gender = genderString === 'female';
+            // } else {
+            // Show error message for invalid gender
+            console.error(
+              'Invalid gender in Excel data. Gender must be "male" or "female".'
+            );
+            this.showGenderErrorMessage = true;
+            setTimeout(() => {
+              this.showGenderErrorMessage = false;
+            }, 3000);
+            event.target.value = null;
+            return; // Exit function if gender is invalid
+          }
+          if (typeof row.name !== 'string' || row.name == null) {
+            console.error('Ivalid name.');
+            this.showNameErrorMessage = true;
+            setTimeout(() => {
+              this.showNameErrorMessage = false;
+            }, 3000);
+            event.target.value = null;
+            return;
+          }
+          if (typeof row.nrc_exists !== 'boolean' || row.nrc_exists == null) {
+            console.error('Invalid NRC.');
+            this.showNRCExistsErrorMessage = true;
+            setTimeout(() => {
+              this.showNRCExistsErrorMessage = false;
+            }, 3000);
+            event.target.value = null;
+            return;
+          }
+        }
+
+        console.log(importedData);
+
+        this.service.bulkCreate(importedData).subscribe({
+          next: (result: any) => {
+            console.log('Excel datas saved successfully', result);
+            this.getstudent();
+            this.showErrorMessage = true;
+            setTimeout(() => {
+              this.showErrorMessage = false;
+            }, 3000);
+            event.target.value = null;
+          },
+          error: (error: any) => {
+            console.log('Error Saving datas form Excel', error);
+            alert('Excel import failed. Please try again.');
+          },
+        });
+      };
+      reader.readAsBinaryString(file);
+    }
+  }
+
+  // ID Validation
+  // validateExcelData(importedData: any[]): boolean {
+  //   const idSet = new Set();
+  //   // const nameSet = new Set();
+  //   for (const row of importedData) {
+  //     // Check if id is a number and is unique
+  //     if (isNaN(row.id) || typeof row.id !== 'number' || idSet.has(row.id)) {
+  //       return false;
+  //     } else {
+  //       console.error(
+  //         'Invalid or duplicate id . Must be a unique number or order ID cannot be null'
+  //       );
+  //       this.showIdErrorMessage = true;
+  //       setTimeout(() => {
+  //         this.showIdErrorMessage = false;
+  //       }, 3000);
+  //     }
+  //     idSet.add(row.id);
+  //     // nameSet.add(row.name.trim().toLowerCase());
+  //   }
+  //   return true;
+  // }
+
+  // Name Validation
+  // validateExcelName(importedData: any[]): string[] {
+  //   const idSet = new Set();
+  //   const errors: string[] = [];
+  //   const nameSet = new Set();
+
+  //   for (const row of importedData) {
+  //     // Check if id is a number and is unique
+  //     if (isNaN(row.id) || typeof row.id !== 'number' || idSet.has(row.id)) {
+  //       errors.push(`Invalid ID for row with name '${row.name}'`);
+  //     }
+  //     // Check if name is a non-empty string and is unique
+  //     if (
+  //       typeof row.name !== 'string' ||
+  //       row.name.trim() === '' ||
+  //       nameSet.has(row.name.trim().toLowerCase())
+  //     ) {
+  //       errors.push(`Invalid name for row with ID '${row.id}'`);
+  //     }
+  //     idSet.add(row.id);
+  //     nameSet.add(row.name.trim().toLowerCase());
+  //   }
+  //   return errors;
+  // }
   // Add
-  async savestudent() {
+  async savestudent(form: any) {
     console.log(this.students);
     var saveData = {
       id: this.students.id,
@@ -57,9 +222,10 @@ export class StudentFormComponent implements OnInit {
     };
     await this.service.studentsApiData(saveData).subscribe({
       next: (result: any) => {
-        console.log('Student add successfully!');
+        console.log('Student adds successfully!');
 
         this.results.push(saveData);
+        form.resetForm();
         // asscending order
         this.results.sort((a: any, b: any) => a.id - b.id);
         this.showSuccessMessage = true;
@@ -67,8 +233,10 @@ export class StudentFormComponent implements OnInit {
           this.showSuccessMessage = false;
         }, 3000);
       },
+
       error: (error: any) => {
         console.log('FAIL', error);
+        alert('Student Id already exists');
       },
     });
   }
@@ -103,6 +271,18 @@ export class StudentFormComponent implements OnInit {
     });
   }
 
+  // Delete all
+  async deleteallstudents() {
+    await this.service.destroystudents(this.students.id).subscribe({
+      next: (result: any) => {
+        console.log('Deleted Sussessfully!');
+        this.getstudent();
+      },
+      error: (error: any) => {
+        alert('FAIL!');
+      },
+    });
+  }
   // Update
   async updatestudent() {
     var updatedata = {
@@ -148,53 +328,15 @@ export class StudentFormComponent implements OnInit {
 
     return age < 18;
   }
-
-  // Export Excel
-
-  exportToExcel(data: any, filenName: string): void {
-    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(data);
-    const wb: XLSX.WorkBook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-    XLSX.writeFile(wb, `${filenName}.xlsx`);
-  }
-
-  // Import Excel
-  importFromExcel(event: any): void {
-    const file: File = event.target.files[0];
-    const reader: FileReader = new FileReader();
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e: any) => {
-        const data = e.target.result;
-        const binaryString: string = e.target.result;
-        const workbook: XLSX.WorkBook = XLSX.read(binaryString, {
-          type: 'binary',
-        });
-        const sheetName: string = workbook.SheetNames[0];
-        const worksheet: XLSX.WorkSheet = workbook.Sheets[sheetName];
-        const importedData: any[] = XLSX.utils.sheet_to_json(worksheet, {
-          raw: true,
-        });
-        // Process the imported data as needed
-        console.log(importedData);
-        // const dataWithUniqueIds = importedData.map(record => {
-        //   return { ...record, Epl_id: uuidv4() };
-        // });
-        this.service.bulkCreate(importedData).subscribe({
-          next: (result: any) => {
-            console.log('Excel datas saved successfully', result);
-            this.getstudent();
-          },
-          error: (error: any) => {
-            console.log('Error Saving datas form Excel', error);
-          },
-        });
-        // Assuming you have a method in your service to handle bulk insertion
-      };
-      reader.readAsBinaryString(file);
+  // Clear NRC
+  clearNrc() {
+    if (this.students.nrc_exists == false) {
+      this.array.nrc == '';
     }
   }
-
+  openFileInput() {
+    this.fileInput.nativeElement.click();
+  }
   toggleNRCInput() {
     throw new Error('Method not implemented.');
   }
